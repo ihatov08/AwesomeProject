831407233712874e1cefadca54c74336df96082d
ihatov08 <ihatov08@gmail.com>
2018-06-18 22:49:19 +0900

    Hello world

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class HelloWorldApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            <Text>Hello World!</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
