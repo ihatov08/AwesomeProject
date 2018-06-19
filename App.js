967a1e08b25094e2e5a3cfbceeffaade89883517
ihatov08 <ihatov08@gmail.com>
2018-06-19 23:22:33 +0900

    height and width

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    setInterval(() => {
-      this.setState(previoutsState => {
-        return { isShowingText: !previoutsState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-          <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text ='Why didi they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+      // <View>
+      //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}></View>
+      //   <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}}></View>
+      //   <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}}></View>
+      // </View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
+        <View style={{flex: 2, backgroundColor: 'skyblue'}}></View>
+        <View style={{flex: 3, backgroundColor: 'steelblue'}}></View>
       </View>
     );
   }
